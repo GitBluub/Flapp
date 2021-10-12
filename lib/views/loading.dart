512ab3cd84713de7516d19dfb88ddc633902a112
arthur.jamet <arthur.jamet@epitech.eu>
2021-10-12 13:30:44 +0200

    manage null values

---
+++ lib/views/loading.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              alignment: Alignment.center,
+              child: const SpinKitRotatingCircle(
+                color: Colors.white,
+                size: 50.0,
+              )
+        ]);
+  }
+}
