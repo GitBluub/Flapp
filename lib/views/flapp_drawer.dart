69b5f83815609f2027ca5b44fce64be93a98c7fb
arthur.jamet <arthur.jamet@epitech.eu>
2021-10-17 15:47:30 +0200

    drawer: if click on same route button, avoid reloading

--- lib/views/flapp_drawer.dart
+++ lib/views/flapp_drawer.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import '../models/redditor.dart';
@@ -8,44 +9,55 @@ class FlappDrawer extends StatelessWidget {
 
   @override
   Widget build(context) {
+    String currentRoute = "";
+
+    currentRoute = ModalRoute.of(context)!.settings.name!;
     return Drawer(
       child: ListView(
         // Important: Remove any padding from the ListView.
         padding: EdgeInsets.zero,
         children: [
           SizedBox(
-              height: 175.0,
+              height: 200.0,
               child: DrawerHeader(
-                  child: Row(children: [
-                CachedNetworkImage(
-                  width: 75,
-                  imageUrl: user.pictureUrl,
-                  imageBuilder: (context, imageProvider) => Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(image: imageProvider)),
-                  ),
-                  placeholder: (context, url) =>
-                      const CircularProgressIndicator(),
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                ),
+                  child: Column(children: [
                 Container(
-                  child: Text(user.name, style: const TextStyle(fontSize: 20)),
-                  padding: const EdgeInsets.only(left: 20),
-                )
+                    height: 100,
+                    child: CachedNetworkImage(
+                      width: 80,
+                      imageUrl: user.pictureUrl,
+                      imageBuilder: (context, imageProvider) => Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(image: imageProvider)),
+                      ),
+                      placeholder: (context, url) =>
+                          const CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    )),
+                Text(user.name, style: const TextStyle(fontSize: 20))
               ]))),
           ListTile(
             leading: const Icon(Icons.home),
             title: const Text('Home'),
             onTap: () {
-              Navigator.pushNamed(context, '/home');
+              if (currentRoute == '/home') {
+                Navigator.pop(context);
+              } else {
+                Navigator.pushNamed(context, '/home');
+              }
             },
           ),
           ListTile(
-            leading: const Icon(Icons.account_circle),
             title: const Text('Profile'),
+            leading: const Icon(Icons.account_circle),
             onTap: () {
-              Navigator.pushNamed(context, '/user');
+              if (currentRoute == '/user') {
+                Navigator.pop(context);
+              } else {
+                Navigator.pushNamed(context, '/user');
+              }
             },
           ),
           ListTile(
