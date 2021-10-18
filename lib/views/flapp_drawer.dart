0cb40002ca7b553566e18bd8b94c312c6f9ac58c
arthur.jamet <arthur.jamet@epitech.eu>
2021-10-18 10:26:41 +0200

    logout option

--- lib/views/flapp_drawer.dart
+++ lib/views/flapp_drawer.dart
@@ -15,69 +15,68 @@ class FlappDrawer extends StatelessWidget {
 
     currentRoute = ModalRoute.of(context)!.settings.name!;
     return Drawer(
-      child: ListView(
-        // Important: Remove any padding from the ListView.
-        padding: EdgeInsets.zero,
-        children: [
-          SizedBox(
-              height: 200.0,
-              child: DrawerHeader(
-                  child: Column(children: [
-                Container(
-                    height: 100,
-                    child: CachedNetworkImage(
-                      width: 80,
-                      imageUrl: user.pictureUrl,
-                      imageBuilder: (context, imageProvider) => Container(
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(image: imageProvider)),
-                      ),
-                      placeholder: (context, url) => const LoadingWidget(),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
-                    )),
-                Text(user.name, style: const TextStyle(fontSize: 20))
-              ]))),
-          ListTile(
-            leading: const Icon(Icons.home),
-            title: const Text('Home'),
-            onTap: () {
-              if (currentRoute == '/home') {
+        child: Column(
+      // Important: Remove any padding from the ListView.
+      children: [
+        SizedBox(
+            height: 200.0,
+            child: DrawerHeader(
+                child: Column(children: [
+              Container(
+                  height: 100,
+                  child: CachedNetworkImage(
+                    width: 80,
+                    imageUrl: user.pictureUrl,
+                    imageBuilder: (context, imageProvider) => Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(image: imageProvider)),
+                    ),
+                    placeholder: (context, url) => const LoadingWidget(),
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
+                  )),
+              Text(user.name, style: const TextStyle(fontSize: 20))
+            ]))),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                if (currentRoute == '/home') {
+                  Navigator.pop(context);
+                } else {
+                  Navigator.pushNamed(context, '/home');
+                }
+              },
+            ),
+            ListTile(
+              title: const Text('Profile'),
+              leading: const Icon(Icons.account_circle),
+              onTap: () {
+                if (currentRoute == '/user') {
+                  Navigator.pop(context);
+                } else {
+                  Navigator.pushNamed(context, '/user');
+                }
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
                 Navigator.pop(context);
-              } else {
-                Navigator.pushNamed(context, '/home');
-              }
-            },
-          ),
-          ListTile(
-            title: const Text('Profile'),
-            leading: const Icon(Icons.account_circle),
-            onTap: () {
-              if (currentRoute == '/user') {
-                Navigator.pop(context);
-              } else {
-                Navigator.pushNamed(context, '/user');
-              }
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings),
-            title: const Text('Settings'),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.sensor_door_outlined),
-            title: const Text('Log out'),
-            onTap: () {
-              GetIt.I<RedditInterface>().stopAPIConnection();
-              Navigator.of(context).popUntil((route) => route.isFirst);
-            },
-          ),
-        ],
-      ),
-    );
+              },
+            ),
+            Expanded(child: Container()),
+            ListTile(
+              leading: const Icon(Icons.sensor_door_outlined),
+              title: const Text('Log out'),
+              onTap: () {
+                GetIt.I<RedditInterface>().stopAPIConnection();
+                Navigator.of(context).popUntil((route) => route.isFirst);
+              },
+            ),
+          ],
+    ));
   }
 }
