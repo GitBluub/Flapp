83d7955b2e127a0091cd70a0f065e9f5c38bf9c9
Bluub <louis.auzuret@gmail.com>
2021-10-29 17:13:37 +0200

    integration testing of the drawer

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,37 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'dart:io' as io;
+import 'package:get_it/get_it.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flapp/src/models/reddit_interface.dart';
 import 'package:flapp/src/main.dart';
+import 'package:flapp/src/models/redditor.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+void main() async {
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+  io.HttpOverrides.global = null;
+  await dotenv.load(fileName: '.env');
+  RedditInterface interface = RedditInterface();
+  GetIt.I.registerSingleton<RedditInterface>(interface);
+  await interface.restoreAPIConnectionTest();
+  testWidgets('Going to profile page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Flapp(connected: false));
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.pumpWidget(Flapp(connected:  GetIt.I<RedditInterface>().connected));
+    final burgerButton = find.byIcon(Icons.menu);
+    expect(burgerButton, findsOneWidget);
+    await tester.tap(burgerButton);
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final drawerHeader = find.byType(DrawerHeader);
+    expect(drawerHeader, findsOneWidget);
+    final searchBtn = find.byIcon(Icons.manage_search);
+    final settingsBtn = find.byIcon(Icons.settings);
+    final homeBtn = find.byIcon(Icons.home);
+    final profileBtn = find.byIcon(Icons.account_circle);
+    expect(searchBtn, findsOneWidget);
+    expect(settingsBtn, findsOneWidget);
+    expect(homeBtn, findsOneWidget);
+    expect(profileBtn, findsOneWidget);
   });
 }
