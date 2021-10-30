db9df3bed97db6bf14fdecc44f6a9948621a5a21
Bluub <louis.auzuret@gmail.com>
2021-10-30 10:12:06 +0200

    adding integration test going to the profile page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,46 @@ import 'package:get_it/get_it.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flapp/src/models/reddit_interface.dart';
 import 'package:flapp/src/main.dart';
+
+import 'package:flapp/src/views/image_header.dart';
 import 'package:flapp/src/models/redditor.dart';
+
+Future checkingDrawer(WidgetTester tester) async {
+  // Build our app and trigger a frame.
+  await tester.pumpWidget(Flapp(connected:  GetIt.I<RedditInterface>().connected));
+  final burgerButton = find.byIcon(Icons.menu);
+  expect(burgerButton, findsOneWidget);
+  await tester.tap(burgerButton);
+  await tester.pump();
+  final drawerHeader = find.byType(DrawerHeader);
+  expect(drawerHeader, findsOneWidget);
+  final searchBtn = find.byIcon(Icons.manage_search);
+  final settingsBtn = find.byIcon(Icons.settings);
+  final homeBtn = find.byIcon(Icons.home);
+  final profileBtn = find.byIcon(Icons.account_circle);
+  expect(searchBtn, findsOneWidget);
+  expect(settingsBtn, findsOneWidget);
+  expect(homeBtn, findsOneWidget);
+  expect(profileBtn, findsOneWidget);
+}
+
+Future goingProfile(WidgetTester tester) async {
+  // Build our app and trigger a frame.
+  await tester.pumpWidget(Flapp(connected:  GetIt.I<RedditInterface>().connected));
+  final burgerButton = find.byIcon(Icons.menu);
+  expect(burgerButton, findsOneWidget);
+  await tester.tap(burgerButton);
+  await tester.pump();
+  final drawerHeader = find.byType(DrawerHeader);
+  expect(drawerHeader, findsOneWidget);
+  final profileBtn = find.byIcon(Icons.account_circle);
+  expect(profileBtn, findsOneWidget);
+  await tester.pump(new Duration(milliseconds: 500));
+  await tester.tap(profileBtn);
+  await tester.pump(new Duration(milliseconds: 500));
+  final imgHeader = find.byType(ImageHeader);
+  expect(imgHeader, findsNothing);
+}
 
 void main() async {
 
@@ -22,22 +61,7 @@ void main() async {
   RedditInterface interface = RedditInterface();
   GetIt.I.registerSingleton<RedditInterface>(interface);
   await interface.restoreAPIConnectionTest();
-  testWidgets('Going to profile page', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(Flapp(connected:  GetIt.I<RedditInterface>().connected));
-    final burgerButton = find.byIcon(Icons.menu);
-    expect(burgerButton, findsOneWidget);
-    await tester.tap(burgerButton);
-    await tester.pump();
-    final drawerHeader = find.byType(DrawerHeader);
-    expect(drawerHeader, findsOneWidget);
-    final searchBtn = find.byIcon(Icons.manage_search);
-    final settingsBtn = find.byIcon(Icons.settings);
-    final homeBtn = find.byIcon(Icons.home);
-    final profileBtn = find.byIcon(Icons.account_circle);
-    expect(searchBtn, findsOneWidget);
-    expect(settingsBtn, findsOneWidget);
-    expect(homeBtn, findsOneWidget);
-    expect(profileBtn, findsOneWidget);
-  });
+
+  testWidgets('Checking drawer buttons', checkingDrawer);
+  testWidgets('Going to profile', goingProfile);
 }
